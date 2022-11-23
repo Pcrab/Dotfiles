fda17d7eef22c16ba6643695e8fff3e37bef3a5b
Pcrab <pcrab@pcrab.xyz>
2022-11-23 17:24:07 +0800

    update: flutter

--- .zsh/0env.zsh
+++ .zsh/0env.zsh
@@ -5,6 +5,8 @@ export XDG_STATE_HOME="${HOME}/.local/state"
 export XDG_RUNTIME_DIR="/run/user/${UID}"
 
 export DOCKER_CONFIG="$XDG_CONFIG_HOME"/docker
+export GRADLE_USER_HOME="$XDG_DATA_HOME"/gradle
+export IPFS_PATH="$XDG_DATA_HOME"/ipfs
 export MACHINE_STORAGE_PATH="$XDG_DATA_HOME"/docker-machine
 export FFMPEG_DATADIR="$XDG_CONFIG_HOME"/ffmpeg
 export GOPATH="$XDG_DATA_HOME"/go
@@ -17,3 +19,7 @@ export TEXMFVAR=$XDG_CACHE_HOME/texlive/texmf-var
 export TEXMFCONFIG=$XDG_CONFIG_HOME/texlive/texmf-config
 
 export EDITOR=nvim
+
+export PUB_HOSTED_URL=https://mirror.sjtu.edu.cn/dart-pub
+export FLUTTER_STORAGE_BASE_URL=https://mirror.sjtu.edu.cn
+
