ab96e202b129f59ed8dac9414b8a899f038b0f60
Pcrab <pcrab@pcrab.xyz>
2022-11-24 20:35:13 +0800

    add backup and restore scripts

--- migrate.sh
+++ migrate.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
-git clone --bare git@github.com:Pcrab/dotfiles.git $HOME/.cfg
-alias cfg='git --git-dir=$HOME/.cfg --work-tree=$HOME'
-cfg config --local status.showUntrackedFiles no
-cfg checkout
+
+# git clone --bare git@github.com:Pcrab/dotfiles.git $HOME/.cfg
+cfg="git --git-dir=$HOME/.cfg --work-tree=$HOME"
+# cfg config --local status.showUntrackedFiles no
+# cfg checkout
+
+for filePath in $($cfg ls-files | grep ".asc$")
+do
+    decryptedFilePath=${filePath%.asc*}
+    gpg -d $filePath > $decryptedFilePath
+done
