0aa5115e1fb00889dbb1cc33face4d82f26db585
Pcrab <pcrab@pcrab.xyz>
2022-12-08 20:59:03 +0800

    feat: add cargo

--- .zsh/rust.zsh
+++ .zsh/rust.zsh
@@ -1,4 +1,5 @@
 export RUSTUP_HOME="$XDG_DATA_HOME"/rustup
+export CARGO_HOME="$XDG_DATA_HOME"/cargo
 export PATH="$XDG_DATA_HOME/rustup/toolchains/stable-x86_64-unknown-linux-gnu/bin:$PATH"
 
 . "$HOME/.cargo/env"
