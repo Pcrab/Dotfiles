dc517d6438d7a73be786946e3aaeb85274c7eed3
Pcrab <pcrab@pcrab.xyz>
2022-12-09 11:37:20 +0800

    fix: fix rust env source path

--- .zsh/rust.zsh
+++ .zsh/rust.zsh
@@ -2,4 +2,4 @@ export RUSTUP_HOME="$XDG_DATA_HOME"/rustup
 export CARGO_HOME="$XDG_DATA_HOME"/cargo
 export PATH="$XDG_DATA_HOME/rustup/toolchains/stable-x86_64-unknown-linux-gnu/bin:$PATH"
 
-. "$HOME/.cargo/env"
+. "$CARGO_HOME/env"
