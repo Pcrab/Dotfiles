5e32adfd2f6535f6fe19bb02021586a8aa38af42
Pcrab <pcrab@pcrab.xyz>
2023-01-03 23:04:40 +0800

    fix: remove unneeded XDG_RUNTIME_DIR

--- .zsh/0env.zsh
+++ .zsh/0env.zsh
@@ -2,7 +2,6 @@ export XDG_CONFIG_HOME="${HOME}/.config"
 export XDG_CACHE_HOME="${HOME}/.cache"
 export XDG_DATA_HOME="${HOME}/.local/share"
 export XDG_STATE_HOME="${HOME}/.local/state"
-export XDG_RUNTIME_DIR="/run/user/${UID}"
 
 export DOCKER_CONFIG="$XDG_CONFIG_HOME"/docker
 export GRADLE_USER_HOME="$XDG_DATA_HOME"/gradle
